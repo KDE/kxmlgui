5381b8b4b76cc1fced0c32aed22653e3a52ebc7a
Kai Uwe Broulik <kde@privat.broulik.de>
2018-02-26 14:57:31 +0100

    Don't show context menu menu if right-clicking outside
    When right-clicking a KXMLGui menu bar entry you can configure it.
    The menu would also show up if right clicked outside the menu which is pointless as none of the actions in the menu
    would work as there's no action to configure to begin with.
    
    BUG: 373653
    
    Differential Revision: https://phabricator.kde.org/D9675

--- src/kmenumenuhandler_p.cpp
+++ src/kmenumenuhandler_p.cpp
@@ -220,8 +220,13 @@ void KMenuMenuHandler::showContextMenu(QMenu *menu, const QPoint &pos)
     Q_ASSERT(!m_popupAction);
     Q_ASSERT(!m_contextMenu);
 
-    m_popupMenu = menu;
     m_popupAction = menu->actionAt(pos);
+
+    if (!m_popupAction) {
+        return;
+    }
+
+    m_popupMenu = menu;
 
     m_contextMenu = new QMenu;
     m_contextMenu->addAction(i18n("Configure Shortcut..."), this, SLOT(slotSetShortcut()));
