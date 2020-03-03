d9d6a3fe43bb45897a0530ce9a5a03f023579d06
Laurent Montel <montel@kde.org>
2020-03-03 14:31:36 +0100

    Make it compile against q5.15. They reverted commit about error() vs networkerror()

--- src/kaboutapplicationpersonmodel_p.cpp
+++ src/kaboutapplicationpersonmodel_p.cpp
@@ -236,11 +236,7 @@ void KAboutApplicationPersonModel::onAvatarJobFinished(QNetworkReply *reply)    
     QNetworkAccessManager *manager = reply->manager();
     int personProfileListIndex = manager->property("personProfile").toInt();
 
-#if (QT_VERSION < QT_VERSION_CHECK(5, 15, 0))
     if (reply->error() != QNetworkReply::NoError) {
-#else
-    if (reply->networkError() != QNetworkReply::NoError) {
-#endif
         //qCDebug(DEBUG_KXMLGUI) << "Could not fetch OCS person avatar.";
         emit dataChanged(index(personProfileListIndex), index(personProfileListIndex));
         return;
@@ -383,11 +379,7 @@ void KAboutApplicationPersonIconsJob::onJobFinished(QNetworkReply *reply)   //SL
     KAboutApplicationPersonProfileOcsLink::Type type = m_ocsLinks.at(i).type();
 
 
-#if (QT_VERSION < QT_VERSION_CHECK(5, 15, 0))
     if (reply->error() != QNetworkReply::NoError) {
-#else
-    if (reply->networkError() != QNetworkReply::NoError) {
-#endif
         //qCDebug(DEBUG_KXMLGUI) << "Could not fetch OCS link icon.";
         reply->deleteLater();
         getIcons(i + 1);
