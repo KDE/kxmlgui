1e9929a59136855b802a40155dcc55fdefd898e9
Laurent Montel <montel@kde.org>
2020-08-31 15:00:40 +0200

    Using no deprecated enum Qt::MiddleButton

--- src/kcheckaccelerators.cpp
+++ src/kcheckaccelerators.cpp
@@ -165,7 +165,7 @@ bool KCheckAccelerators::eventFilter(QObject *obj, QEvent *e)
         break;
     //case QEvent::MouseButtonDblClick:
     case QEvent::MouseButtonPress:
-        if (copyWidgetText && static_cast<QMouseEvent *>(e)->button() == Qt::MidButton) {
+        if (copyWidgetText && static_cast<QMouseEvent *>(e)->button() == Qt::MiddleButton) {
             //kWarning()<<"obj"<<obj;
             QWidget *w = static_cast<QWidget *>(obj)->childAt(static_cast<QMouseEvent *>(e)->pos());
             if (!w) {
