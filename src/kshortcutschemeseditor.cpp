KDE/kxmlgui

commit de0790feb31cb021892e5a599d93d1a4fb476201

[KShortcutsDialog] Add icons for actions

src/kshortcutschemeseditor.cpp
@@ -70,20 +70,28 @@ KShortcutSchemesEditor::KShortcutSchemesEditor(KShortcutsDialog *parent)
     schemesLabel->setBuddy(m_schemesList);
     l->addWidget(m_schemesList);
 
-    m_newScheme = new QPushButton(i18nc("@action:button", "New..."));
+    m_newScheme = new QPushButton(QIcon::fromTheme(QStringLiteral("document-new")), i18nc("@action:button", "New..."));
     l->addWidget(m_newScheme);
 
-    m_deleteScheme = new QPushButton(i18nc("@action:button", "Delete"));
+    m_deleteScheme = new QPushButton(QIcon::fromTheme(QStringLiteral("edit-delete")), i18nc("@action:button", "Delete"));
     l->addWidget(m_deleteScheme);
 
-    QPushButton *moreActions = new QPushButton(i18nc("@action:button", "More Actions"));
+    QPushButton *moreActions = new QPushButton(QIcon::fromTheme(QStringLiteral("view-more-symbolic")), i18nc("@action:button", "More Actions"));
     l->addWidget(moreActions);
 
     QMenu *moreActionsMenu = new QMenu(this);
-    moreActionsMenu->addAction(i18nc("@action:inmenu", "Save shortcuts to scheme"), this, &KShortcutSchemesEditor::saveAsDefaultsForScheme);
-    moreActionsMenu->addAction(i18nc("@action:inmenu", "Export Scheme..."), this, &KShortcutSchemesEditor::exportShortcutsScheme);
-    moreActionsMenu->addAction(i18nc("@action:inmenu", "Import Scheme..."), this, &KShortcutSchemesEditor::importShortcutsScheme);
-    moreActions->setMenu(moreActionsMenu);
+    moreActionsMenu->addAction(QIcon::fromTheme(QStringLiteral("document-save")),
+                               i18nc("@action:inmenu", "Save shortcuts to scheme"),
+                               this,
+                               &KShortcutSchemesEditor::saveAsDefaultsForScheme);
+    moreActionsMenu->addAction(QIcon::fromTheme(QStringLiteral("document-export")),
+                               i18nc("@action:inmenu", "Export Scheme..."),
+                               this,
+                               &KShortcutSchemesEditor::exportShortcutsScheme);
+    moreActionsMenu->addAction(QIcon::fromTheme(QStringLiteral("document-import")),
+                               i18nc("@action:inmenu", "Import Scheme..."),
+                               this,
+                               &KShortcutSchemesEditor::importShortcutsScheme);
 
     l->addStretch(1);
 

commit 52eb340c572ae47d71b6f3f8f61f950e29368c3c

[KShortcutSchemesEditor] Fix bug introduced by refactoring in commit de0790fe
Hello there :),

This is a very simple fix for a small refactoring mistake that happened in commit de0790fe (line 86 shouldn't have been deleted).

Cheers,

src/kshortcutschemeseditor.cpp
@@ -92,6 +92,7 @@ KShortcutSchemesEditor::KShortcutSchemesEditor(KShortcutsDialog *parent)
                                i18nc("@action:inmenu", "Import Scheme..."),
                                this,
                                &KShortcutSchemesEditor::importShortcutsScheme);
+    moreActions->setMenu(moreActionsMenu);
 
     l->addStretch(1);
 