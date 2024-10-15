KDE/kxmlgui

commit 4eb7098a7b7b878b328b76c58d37607e75073d60

Set default state config group in KMainWindow
This used to be opt-in in KF5, but it is reasonable to have it as the default in KF6

Consumers may still change the name of the group that should be used

src/kmainwindow.cpp
@@ -51,8 +51,6 @@
 #include <KStandardShortcut>
 #include <KWindowConfig>
 
-//#include <cctype>
-
 static QMenuBar *internalMenuBar(KMainWindow *mw)
 {
     return mw->findChild<QMenuBar *>(QString(), Qt::FindDirectChildrenOnly);
@@ -530,7 +528,7 @@ void KMainWindow::closeEvent(QCloseEvent *e)
     // Delete the marker that says we don't want to restore the position of the
     // next-opened instance; now that a window is closing, we do want to do this
     if (d->autoSaveGroup.isValid()) {
-        d->getValidStateConfig(d->autoSaveGroup).deleteEntry("RestorePositionForNextInstance");
+        d->getStateConfig().deleteEntry("RestorePositionForNextInstance");
     }
     d->_k_slotSaveAutoSavePosition();
 
@@ -591,17 +589,16 @@ void KMainWindow::saveMainWindowSettings(KConfigGroup &cg)
     Q_D(KMainWindow);
     // qDebug(200) << "KMainWindow::saveMainWindowSettings " << cg.name();
 
-    KConfigGroup stateConfig = d->getValidStateConfig(cg);
     // Called by session management - or if we want to save the window size anyway
     if (d->autoSaveWindowSize) {
-        KWindowConfig::saveWindowSize(windowHandle(), stateConfig);
-        KWindowConfig::saveWindowPosition(windowHandle(), stateConfig);
+        KWindowConfig::saveWindowSize(windowHandle(), d->getStateConfig());
+        KWindowConfig::saveWindowPosition(windowHandle(), d->getStateConfig());
     }
 
     // One day will need to save the version number, but for now, assume 0
     // Utilise the QMainWindow::saveState() functionality.
     const QByteArray state = saveState();
-    stateConfig.writeEntry("State", state.toBase64());
+    d->getStateConfig().writeEntry("State", state.toBase64());
 
     QStatusBar *sb = internalStatusBar(this);
     if (sb) {
@@ -687,7 +684,7 @@ void KMainWindow::applyMainWindowSettings(const KConfigGroup &_cg)
     const bool oldLetDirtySettings = d->letDirtySettings;
     d->letDirtySettings = false;
 
-    KConfigGroup stateConfig = d->getValidStateConfig(cg);
+    KConfigGroup stateConfig = d->getStateConfig();
 
     if (!d->sizeApplied && (!window() || window() == this)) {
         winId(); // ensure there's a window created
@@ -846,7 +843,7 @@ void KMainWindow::saveAutoSaveSettings()
     // qDebug(200) << "KMainWindow::saveAutoSaveSettings -> saving settings";
     saveMainWindowSettings(d->autoSaveGroup);
     d->autoSaveGroup.sync();
-    d->autoSaveStateGroup().sync();
+    d->m_stateConfigGroup.sync();
     d->settingsDirty = false;
 }
 
@@ -946,14 +943,14 @@ void KMainWindowPrivate::_k_slotSettingsChanged(int category)
 void KMainWindowPrivate::_k_slotSaveAutoSaveSize()
 {
     if (autoSaveGroup.isValid()) {
-        KWindowConfig::saveWindowSize(q->windowHandle(), autoSaveStateGroup());
+        KWindowConfig::saveWindowSize(q->windowHandle(), getStateConfig());
     }
 }
 
 void KMainWindowPrivate::_k_slotSaveAutoSavePosition()
 {
     if (autoSaveGroup.isValid()) {
-        KWindowConfig::saveWindowPosition(q->windowHandle(), autoSaveStateGroup());
+        KWindowConfig::saveWindowPosition(q->windowHandle(), getStateConfig());
     }
 }
 

src/kmainwindow_p.h
@@ -15,6 +15,7 @@
 #define KMAINWINDOW_P_H
 
 #include <KConfigGroup>
+#include <KSharedConfig>
 #include <QEventLoopLocker>
 #include <QPointer>
 
@@ -36,15 +37,15 @@ public:
     bool suppressCloseEvent : 1;
 
     KConfigGroup autoSaveGroup;
-    // If API consumers opt-in to save the state config separately, we want to save the window sizes in the given config
-    KConfigGroup &autoSaveStateGroup()
-    {
-        return m_stateConfigGroup.isValid() ? m_stateConfigGroup : autoSaveGroup;
-    }
     KConfigGroup m_stateConfigGroup;
-    inline KConfigGroup getValidStateConfig(KConfigGroup &cg) const
+
+    inline KConfigGroup &getStateConfig()
     {
-        return m_stateConfigGroup.isValid() ? m_stateConfigGroup : cg;
+        if (!m_stateConfigGroup.isValid()) {
+            // Always use a separate state config here, consumers may override this with a custom/window-specific group
+            m_stateConfigGroup = KSharedConfig::openStateConfig()->group(QStringLiteral("State"));
+        }
+        return m_stateConfigGroup;
     }
     inline void migrateStateDataIfNeeded(KConfigGroup &cg)
     {

commit 681a6014619b45aa7fa248ae60c0e3dbd673ca84

Fix window position not being restored
Seems like a regression from 4eb7098a7b7b878b328b76c58d37607e75073d60

BUG: 493401

src/kmainwindow.cpp
@@ -512,7 +512,7 @@ void KMainWindow::closeEvent(QCloseEvent *e)
     }
     // Delete the marker that says we don't want to restore the position of the
     // next-opened instance; now that a window is closing, we do want to do this
-    if (d->autoSaveGroup.isValid()) {
+    if (d->getStateConfig().isValid()) {
         d->getStateConfig().deleteEntry("RestorePositionForNextInstance");
     }
     d->_k_slotSaveAutoSavePosition();