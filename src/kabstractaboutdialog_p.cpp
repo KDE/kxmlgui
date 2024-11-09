737cf65271c144b75b8abc7d71bea6e6bf077e13
Carl Schwan <carl@carlschwan.eu>
2024-11-09 20:19:38 +0100

    Revert "about dialog: Put app specific components before generic components"
    This reverts commit 4a3f49470c5c880af7ac8a6c9a509de4fa967914.
    
    Was pushed to the wrong branch

--- src/kabstractaboutdialog_p.cpp
+++ src/kabstractaboutdialog_p.cpp
@@ -109,19 +109,17 @@ QWidget *KAbstractAboutDialogPrivate::createComponentWidget(const QList<KAboutCo
     QList<KAboutComponent> allComponents = components;
     auto platform = QGuiApplication::platformName();
     platform.replace(0, 1, platform[0].toUpper());
-    allComponents.append(KAboutComponent(i18n("KDE Frameworks"),
-                                         i18nc("@info", "Collection of libraries created by the KDE Community to extend Qt"),
-                                         QStringLiteral(KXMLGUI_VERSION_STRING),
-                                         QStringLiteral("https://develop.kde.org/products/frameworks/"),
-                                         KAboutLicense::LGPL_V2_1));
-
-    allComponents.append(KAboutComponent(i18n("Qt"),
-                                         i18nc("@info", "Cross-platform application development framework"),
-                                         i18n("Using %1 and built against %2", QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR)),
-                                         QStringLiteral("https://www.qt.io/"),
-                                         KAboutLicense::LGPL_V3));
-
-    allComponents.append(KAboutComponent(platform, i18n("Windowing system")));
+    allComponents.prepend(KAboutComponent(platform, i18n("Windowing system")));
+    allComponents.prepend(KAboutComponent(i18n("Qt"),
+                                          i18nc("@info", "Cross-platform application development framework"),
+                                          i18n("Using %1 and built against %2", QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR)),
+                                          QStringLiteral("https://www.qt.io/"),
+                                          KAboutLicense::LGPL_V3));
+    allComponents.prepend(KAboutComponent(i18n("KDE Frameworks"),
+                                          i18nc("@info", "Collection of libraries created by the KDE Community to extend Qt"),
+                                          QStringLiteral(KXMLGUI_VERSION_STRING),
+                                          QStringLiteral("https://develop.kde.org/products/frameworks/"),
+                                          KAboutLicense::LGPL_V2_1));
 
     for (qsizetype i = 0, count = allComponents.count(); i < count; i++) {
         const auto &component = allComponents[i];
