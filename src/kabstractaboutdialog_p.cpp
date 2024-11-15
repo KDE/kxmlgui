2dfcae63188045a0e92f47c3e722a2f9f939d55e
Carl Schwan <carl@carlschwan.eu>
2024-11-15 12:04:00 +0100

    AboutDialog: Add copy button for components info
    And sync the description of the components with the kirigami addons
    version.

--- src/kabstractaboutdialog_p.cpp
+++ src/kabstractaboutdialog_p.cpp
@@ -24,10 +24,13 @@
 // Qt
 #include <QApplication>
 #include <QCheckBox>
+#include <QClipboard>
 #include <QDesktopServices>
 #include <QDialogButtonBox>
 #include <QIcon>
 #include <QLabel>
+#include <QPushButton>
+#include <QStyle>
 #include <QToolButton>
 #include <QVBoxLayout>
 
@@ -107,21 +110,26 @@ QWidget *KAbstractAboutDialogPrivate::createComponentWidget(const QList<KAboutCo
     wrapper->setWidget(componentWidget);
 
     QList<KAboutComponent> allComponents = components;
+    QString platformName;
     auto platform = QGuiApplication::platformName();
     platform.replace(0, 1, platform[0].toUpper());
+    if (platform == u"Wayland"_s || platform == u"Xcb"_s) {
+        platform = i18nc("@info Platform name", "%1 (%2)", QSysInfo::prettyProductName(), platform);
+    } else {
+        platform = QSysInfo::prettyProductName();
+    }
+
     allComponents.append(KAboutComponent(i18n("KDE Frameworks"),
-                                         i18nc("@info", "Collection of libraries created by the KDE Community to extend Qt"),
+                                         i18nc("@info", "Collection of libraries created by the KDE Community to extend Qt."),
                                          QStringLiteral(KXMLGUI_VERSION_STRING),
                                          QStringLiteral("https://develop.kde.org/products/frameworks/"),
                                          KAboutLicense::LGPL_V2_1));
-
     allComponents.append(KAboutComponent(i18n("Qt"),
-                                         i18nc("@info", "Cross-platform application development framework"),
+                                         i18nc("@info", "Cross-platform application development framework."),
                                          i18n("Using %1 and built against %2", QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR)),
                                          QStringLiteral("https://www.qt.io/"),
                                          KAboutLicense::LGPL_V3));
-
-    allComponents.append(KAboutComponent(platform, i18n("Windowing system")));
+    allComponents.append(KAboutComponent(platform, i18nc("@info", "Underlying platform.")));
 
     for (qsizetype i = 0, count = allComponents.count(); i < count; i++) {
         const auto &component = allComponents[i];
@@ -176,6 +184,31 @@ QWidget *KAbstractAboutDialogPrivate::createComponentWidget(const QList<KAboutCo
             componentLayout->addWidget(separator);
         }
     }
+
+    componentLayout->addSpacing(parent->style()->pixelMetric(QStyle::PM_LayoutVerticalSpacing));
+
+    auto copyButton = new QPushButton(QIcon::fromTheme(u"edit-copy-symbolic"_s), i18nc("@action:button", "Copy to Clipboard"), parent);
+    copyButton->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Preferred);
+    QObject::connect(copyButton, &QPushButton::clicked, copyButton, [allComponents]() {
+        auto aboutData = KAboutData::applicationData();
+        QString info = aboutData.displayName() + u": "_s + aboutData.version() + u'\n';
+
+        for (const auto &component : allComponents) {
+            info += component.name();
+            if (!component.version().isEmpty()) {
+                info += u": "_s + component.version();
+            }
+            info += u'\n';
+        }
+
+        info += u"Build ABI: "_s + QSysInfo::buildAbi() + u'\n';
+        info += u"Kernel: "_s + QSysInfo::kernelType() + u' ' + QSysInfo::kernelVersion() + u'\n';
+
+        QClipboard *clipboard = QGuiApplication::clipboard();
+        clipboard->setText(info);
+    });
+
+    componentLayout->addWidget(copyButton);
 
     componentLayout->addStretch();
 
