c06a6c23f0a6b6fc4ca2d75caf0f8287c1c32684
Nicolas Fella <nicolas.fella@gmx.de>
2024-11-26 21:19:49 +0100

    Add Python bindings
    Co-authored-by: Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>

---
+++ python/bindings.h
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2024 Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#pragma once
+
+#include <QMetaMethod>
+
+#include <KAboutApplicationDialog>
+#include <KAboutPluginDialog>
+#include <KActionCategory>
+#include <KActionCollection>
+#include <KBugReport>
+#include <KEditToolBar>
+#include <KHelpMenu>
+#include <KKeySequenceWidget>
+#include <KMainWindow>
+#include <KShortcutWidget>
+#include <KShortcutsDialog>
+#include <KShortcutsEditor>
+#include <KToggleToolBarAction>
+#include <KToolBar>
+#include <KToolTipHelper>
+#include <KUndoActions>
+#include <KXMLGUIBuilder>
+#include <KXMLGUIClient>
+#include <KXMLGUIFactory>
+#include <KXmlGuiWindow>
