d1d64e23c80df950c5d315828504480499a0a574
Christoph Cullmann <christoph@cullmann.io>
2025-01-21 20:04:04 +0100

    instantiate accelerator checking only if we create main windows, too
    avoids old heuristics and ensures not done via plugins

--- src/kcheckaccelerators.cpp
+++ src/kcheckaccelerators.cpp
@@ -35,84 +35,6 @@
 #include <KLocalizedString>
 #include <KSharedConfig>
 
-class KCheckAcceleratorsInitializer : public QObject
-{
-    Q_OBJECT
-public:
-    explicit KCheckAcceleratorsInitializer(QObject *parent = nullptr)
-        : QObject(parent)
-    {
-    }
-
-public Q_SLOTS:
-    void initiateIfNeeded()
-    {
-        KConfigGroup cg(KSharedConfig::openConfig(), QStringLiteral("Development"));
-        QString sKey = cg.readEntry("CheckAccelerators").trimmed();
-        int key = 0;
-        if (!sKey.isEmpty()) {
-            QList<QKeySequence> cuts = QKeySequence::listFromString(sKey);
-            if (!cuts.isEmpty()) {
-                key = cuts.first()[0].toCombined();
-            }
-        }
-        const bool autoCheck = cg.readEntry("AutoCheckAccelerators", true);
-        if (key == 0 && !autoCheck) {
-            deleteLater();
-            return;
-        }
-
-        new KCheckAccelerators(qApp, key, autoCheck);
-        deleteLater();
-    }
-};
-
-static void startupFunc()
-{
-    // Static because in some cases this is called multiple times
-    // but if an application had any of the bad cases we always want
-    // to skip the check
-    static bool doCheckAccelerators = true;
-
-    if (!doCheckAccelerators) {
-        return;
-    }
-
-    QCoreApplication *app = QCoreApplication::instance();
-    if (!app) {
-        // We're being loaded by something that doesn't have a QCoreApplication
-        // this would probably crash at some later point since we do use qApp->
-        // quite a lot, so skip the magic
-        doCheckAccelerators = false;
-        return;
-    }
-
-    if (!QCoreApplication::startingUp()) {
-        // If the app has already started, this means we're not being run as part of
-        // qt_call_pre_routines, which most probably means that we're being run as
-        // part of KXmlGui being loaded as part of some plugin of the app, so don't
-        // do any magic
-        doCheckAccelerators = false;
-        return;
-    }
-
-    if (!QCoreApplication::eventDispatcher()) {
-        // We are called with event dispatcher being null when KXmlGui is being
-        // loaded through plasma-integration instead of being linked to the app
-        // (i.e. QtCreator vs Okular) For apps that don't link directly to KXmlGui
-        // do not do the accelerator magic
-        doCheckAccelerators = false;
-        return;
-    }
-
-    KCheckAcceleratorsInitializer *initializer = new KCheckAcceleratorsInitializer(app);
-    // Call initiateIfNeeded once we're in the event loop
-    // This is to prevent using KSharedConfig before main() can set the app name
-    QMetaObject::invokeMethod(initializer, "initiateIfNeeded", Qt::QueuedConnection);
-}
-
-Q_COREAPP_STARTUP_FUNCTION(startupFunc)
-
 KCheckAccelerators::KCheckAccelerators(QObject *parent, int key_, bool autoCheck_)
     : QObject(parent)
     , key(key_)
@@ -273,5 +195,28 @@ void KCheckAccelerators::checkAccelerators(bool automatic)
     // dlg will be destroyed before returning
 }
 
-#include "kcheckaccelerators.moc"
+void KCheckAccelerators::initiateIfNeeded()
+{
+    static QPointer<KCheckAccelerators> checker;
+    if (checker) {
+        return;
+    }
+
+    KConfigGroup cg(KSharedConfig::openConfig(), QStringLiteral("Development"));
+    QString sKey = cg.readEntry("CheckAccelerators").trimmed();
+    int key = 0;
+    if (!sKey.isEmpty()) {
+        QList<QKeySequence> cuts = QKeySequence::listFromString(sKey);
+        if (!cuts.isEmpty()) {
+            key = cuts.first()[0].toCombined();
+        }
+    }
+    const bool autoCheck = cg.readEntry("AutoCheckAccelerators", true);
+    if (key == 0 && !autoCheck) {
+        return;
+    }
+
+    checker = new KCheckAccelerators(qApp, key, autoCheck);
+}
+
 #include "moc_kcheckaccelerators.cpp"
